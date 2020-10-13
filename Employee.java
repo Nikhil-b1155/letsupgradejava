742cd2b10f0720c58c3a66e89c2e2f9214177672
Nikhil-b1155 <72803852+Nikhil-b1155@users.noreply.github.com>
2020-10-13 18:30:47 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,11 @@
+package com.company;
+public class Employee { //Employee class
+    String name ;
+    int age ;
+    String city ;
+    public void display(){
+        System.out.println("The name of employee is "+name+".");
+        System.out.println("The age of employee is "+age+".");
+        System.out.println("The city of employee is "+city+".");
+    }
+}
---
+++ Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Employee e1 = new Employee();
+        e1.name="Nikhil";
+        e1.age=20;
+        e1.city="Pune";
+        e1.display();
+
+        Employee e2 = new Employee();
+        e2.name="Mayur";
+        e2.age=20;
+        e2.city="Pune";
+        e2.display();
+    }
+}
